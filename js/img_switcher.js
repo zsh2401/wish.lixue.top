fb07feebf1f4fbddf9e187635d2c4bc0aa13d81b
zsh2401 <zsh2401@163.com>
2017-12-31 17:46:50 +0800

    ...

---
+++ js/img_switcher.js
@@ -0,0 +1,18 @@
+var imgs = ["a.jpg","b.jpg","c.jpg"];
+var img_path = "img/";
+var img_id = "header_img";
+var currentIndex=1;
+var maxIndex=imgs.length; 
+function autoSwitch(){
+   
+    if(currentIndex >= maxIndex){
+        currentIndex = 0;
+    }
+    setImg(imgs[currentIndex]);
+    currentIndex++;
+}
+function setImg(imgname){
+    fileName = img_path + imgname;
+    var imgObj = document.getElementById(img_id);
+    imgObj.src=fileName;
+}
