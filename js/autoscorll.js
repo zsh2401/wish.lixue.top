99f42ea34226e0ce4108cc48f26fce650500ddd7
zsh2401 <zsh2401@163.com>
2018-01-01 06:31:28 +0800

    ...

--- js/autoscorll.js
+++ js/autoscorll.js
@@ -1,12 +1,36 @@
-function pageHeight(){
-    return document.body.scrollHeight;  
+/*
+autoscorll.js
+coding by zsh2401 on 01/01/2018
+need
+jquery
+jquery-mobile
+*/
+var _as_timer;
+as_check();
+as_event_init();
+autoscorll_start();
+function as_check(){
+    if(typeof jQuery =='undefined'){
+        alert("jQuery library is not found!");
+    }
 }
-function current(){
-    return document.body.scrollTop;   
+function as_event_init(){
+    $(document).on("tapon",function(){
+        alert("xxxx");
+     });
 }
-var timer;
-timer=setInterval("scorllDown()",10);
-function scorllDown(){
-    num = current() + 1;
-    scroll(0,num);
+function autoscorll_start(){
+    _as_timer=setInterval("__scorllDown()",10);
+}
+function autoscorll_stop(){
+    clearInterval(_as_timer);
+}
+function __scorllDown(){
+    var _crt = document.documentElement.scrollTop;
+    var _height = document.body.scrollHeight; 
+    if(_crt<_height){
+        scroll(0,_crt + 1);
+    }else{
+        autoscorll_stop();
+    }
 }
