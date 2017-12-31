bf55ec19c540cc21051ca61f735d28aeeb996d44
zsh2401 <zsh2401@163.com>
2017-12-31 18:05:19 +0800

    ...

--- js/img_switcher.js
+++ js/img_switcher.js
@@ -4,16 +4,15 @@ var img_id = "header_img";
 var currentIndex=1;
 var maxIndex=imgs.length; 
 function autoSwitch(){
-   
     if(currentIndex >= maxIndex){
         currentIndex = 0;
     }
     setImg(imgs[currentIndex]);
     currentIndex++;
-    
 }
 function setImg(imgname){
     fileName = img_path + imgname;
     var imgObj = document.getElementById(img_id);
     imgObj.src=fileName;
+    imgObj.Height = document.body.clientWidth / 2;
 }
